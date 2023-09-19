1d908d63d531c253ef8d4fd0ae85307c628ad4c9
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2023-09-19 12:53:28 -0400

    feat: add route to bring all players

--- src/app/api/player/route.ts
+++ src/app/api/player/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "../../lib/prisma";
 
 export async function GET() {
-  //   const players = await prisma.players.findMany();
+  const players = await prisma.players.findMany();
 
-  return NextResponse.json("<h1>hello wolrd</h1>", { status: 200 });
+  return new NextResponse(JSON.stringify(players), { status: 200 });
 }
